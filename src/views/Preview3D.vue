2dd5ca19f0f83245337b11a5c43e0598fbfd61b5
Nicolas Brown <nclsbrwn@gmail.com>
2019-09-23 18:08:57 -0500

    Select default tiling in 3d preview

--- src/views/Preview3D.vue
+++ src/views/Preview3D.vue
@@ -16,7 +16,7 @@
         <option value="1">Tiling: 1x</option>
         <option value="2">Tiling: 2x</option>
         <option value="3">Tiling: 3x</option>
-        <option value="4">Tiling: 4x</option>
+        <option value="4" selected>Tiling: 4x</option>
       </select>
     </div>
     <canvas id="_3dpreview" ref="canvas" style="display:block;"></canvas>
