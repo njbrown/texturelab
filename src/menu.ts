d88dfb200f9e664350177c61a08e6c51706a36a8
Nicolas Brown <nclsbrwn@gmail.com>
2019-10-27 16:29:29 -0500

    Remove samples menu

--- src/menu.ts
+++ src/menu.ts
@@ -6,152 +6,152 @@
 const { app, Menu, BrowserWindow } = require("electron");
 
 export enum MenuCommands {
-	FileNew = "file_new",
-	FileOpen = "file_open",
-	FileSave = "file_save",
-	FileSaveAs = "file_saveas",
-	FileExit = "file_exit",
+  FileNew = "file_new",
+  FileOpen = "file_open",
+  FileSave = "file_save",
+  FileSaveAs = "file_saveas",
+  FileExit = "file_exit",
 
-	ExportZip = "export_zip",
-	ExportUnity = "export_unity",
-	ExportUnityZip = "export_unity_zip",
+  ExportZip = "export_zip",
+  ExportUnity = "export_unity",
+  ExportUnityZip = "export_unity_zip",
 
-	SamplesBrick = "samples_brick",
+  SamplesBrick = "samples_brick",
 
-	HelpTutorials = "help_tutorials",
-	HelpAbout = "help_about",
-	HelpSubmitBug = "help_submitbug"
+  HelpTutorials = "help_tutorials",
+  HelpAbout = "help_about",
+  HelpSubmitBug = "help_submitbug"
 }
 
 export function setupMenu() {
-	const template = [
-		{
-			label: "File",
-			submenu: [
-				{
-					label: "New",
-					accelerator: "CmdOrCtrl+N",
-					click: (item, focusedWindow) => {
-						focusedWindow.webContents.send(MenuCommands.FileNew);
-					}
-				},
-				{
-					label: "Open",
-					accelerator: "CmdOrCtrl+O",
-					click: (item, focusedWindow) => {
-						focusedWindow.webContents.send(MenuCommands.FileOpen);
-					}
-				},
-				{
-					label: "Save",
-					accelerator: "CmdOrCtrl+S",
-					click: (item, focusedWindow) => {
-						focusedWindow.webContents.send(MenuCommands.FileSave);
-					}
-				},
-				{
-					label: "Save As..",
-					accelerator: "CmdOrCtrl+Shift+S",
-					click: (item, focusedWindow) => {
-						focusedWindow.webContents.send(MenuCommands.FileSaveAs);
-					}
-				},
-				{
-					label: "Exit",
-					click: (item, focusedWindow) => {
-						focusedWindow.webContents.send(MenuCommands.FileExit);
-					}
-				}
-			]
-		},
-		{
-			label: "Export",
-			submenu: [
-				{
-					label: "Zip",
-					click: (item, focusedWindow) => {
-						focusedWindow.webContents.send(MenuCommands.ExportZip);
-					}
-				},
-				// {
-				//   label: "Unity Material",
-				//   click: (item, focusedWindow) => {
-				//     focusedWindow.webContents.send(MenuCommands.ExportUnity);
-				//   }
-				// },
-				{
-					label: "Unity (Zip)",
-					click: (item, focusedWindow) => {
-						focusedWindow.webContents.send(MenuCommands.ExportUnityZip);
-					}
-				}
-			]
-		},
-		{
-			label: "Samples",
-			submenu: [
-				{
-					label: "Lava"
-				},
-				{
-					label: "Water"
-				},
-				{
-					label: "Wood"
-				}
-			]
-		},
-		{
-			label: "Help",
-			submenu: [
-				{
-					label: "Tutorials"
-				},
-				{
-					label: "About"
-				},
-				{
-					label: "Submit Bug"
-				}
-			]
-		},
-		...(process.env.NODE_ENV !== "production"
-			? [
-					{
-						label: "Dev",
-						submenu: [
-							{ role: "reload" },
-							{
-								role: "forcereload",
-								click: function(item, focusedWindow) {
-									if (focusedWindow) {
-										// After overloading, refresh and close all secondary forms
-										if (focusedWindow.id === 1) {
-											BrowserWindow.getAllWindows().forEach(function(win) {
-												if (win.id > 1) {
-													win.close();
-												}
-											});
-										}
-										focusedWindow.reload();
-									}
-								}
-							},
-							{ role: "toggledevtools" },
-							{ type: "separator" },
-							{ role: "resetzoom" },
-							{ role: "zoomin" },
-							{ role: "zoomout" },
-							{ type: "separator" },
-							{ role: "togglefullscreen" }
-						]
-					}
-			  ]
-			: [])
-	];
+  const template = [
+    {
+      label: "File",
+      submenu: [
+        {
+          label: "New",
+          accelerator: "CmdOrCtrl+N",
+          click: (item, focusedWindow) => {
+            focusedWindow.webContents.send(MenuCommands.FileNew);
+          }
+        },
+        {
+          label: "Open",
+          accelerator: "CmdOrCtrl+O",
+          click: (item, focusedWindow) => {
+            focusedWindow.webContents.send(MenuCommands.FileOpen);
+          }
+        },
+        {
+          label: "Save",
+          accelerator: "CmdOrCtrl+S",
+          click: (item, focusedWindow) => {
+            focusedWindow.webContents.send(MenuCommands.FileSave);
+          }
+        },
+        {
+          label: "Save As..",
+          accelerator: "CmdOrCtrl+Shift+S",
+          click: (item, focusedWindow) => {
+            focusedWindow.webContents.send(MenuCommands.FileSaveAs);
+          }
+        },
+        {
+          label: "Exit",
+          click: (item, focusedWindow) => {
+            focusedWindow.webContents.send(MenuCommands.FileExit);
+          }
+        }
+      ]
+    },
+    {
+      label: "Export",
+      submenu: [
+        {
+          label: "Zip",
+          click: (item, focusedWindow) => {
+            focusedWindow.webContents.send(MenuCommands.ExportZip);
+          }
+        },
+        // {
+        //   label: "Unity Material",
+        //   click: (item, focusedWindow) => {
+        //     focusedWindow.webContents.send(MenuCommands.ExportUnity);
+        //   }
+        // },
+        {
+          label: "Unity (Zip)",
+          click: (item, focusedWindow) => {
+            focusedWindow.webContents.send(MenuCommands.ExportUnityZip);
+          }
+        }
+      ]
+    },
+    // {
+    // 	label: "Samples",
+    // 	submenu: [
+    // 		{
+    // 			label: "Lava"
+    // 		},
+    // 		{
+    // 			label: "Water"
+    // 		},
+    // 		{
+    // 			label: "Wood"
+    // 		}
+    // 	]
+    // },
+    {
+      label: "Help",
+      submenu: [
+        {
+          label: "Tutorials"
+        },
+        {
+          label: "About"
+        },
+        {
+          label: "Submit Bug"
+        }
+      ]
+    },
+    ...(process.env.NODE_ENV !== "production"
+      ? [
+          {
+            label: "Dev",
+            submenu: [
+              { role: "reload" },
+              {
+                role: "forcereload",
+                click: function(item, focusedWindow) {
+                  if (focusedWindow) {
+                    // After overloading, refresh and close all secondary forms
+                    if (focusedWindow.id === 1) {
+                      BrowserWindow.getAllWindows().forEach(function(win) {
+                        if (win.id > 1) {
+                          win.close();
+                        }
+                      });
+                    }
+                    focusedWindow.reload();
+                  }
+                }
+              },
+              { role: "toggledevtools" },
+              { type: "separator" },
+              { role: "resetzoom" },
+              { role: "zoomin" },
+              { role: "zoomout" },
+              { type: "separator" },
+              { role: "togglefullscreen" }
+            ]
+          }
+        ]
+      : [])
+  ];
 
-	console.log(template);
+  //console.log(template);
 
-	const menu = Menu.buildFromTemplate(template as any);
-	Menu.setApplicationMenu(menu);
+  const menu = Menu.buildFromTemplate(template as any);
+  Menu.setApplicationMenu(menu);
 }
