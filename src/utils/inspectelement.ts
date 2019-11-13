750b77883e7d4a1e240c27e9086494a3f5d6ca27
Nicolas Brown <nclsbrwn@gmail.com>
2019-11-13 02:31:08 -0500

    Remove inspect element from production

--- src/utils/inspectelement.ts
+++ src/utils/inspectelement.ts
@@ -3,24 +3,25 @@ const Menu = remote.Menu;
 const MenuItem = remote.MenuItem;
 
 let rightClickPosition = null;
+if (process.env.NODE_ENV != "production") {
+  const menu = new remote.Menu();
+  const menuItem = new MenuItem({
+    label: "Inspect Element",
+    click: () => {
+      remote
+        .getCurrentWindow()
+        .webContents.inspectElement(rightClickPosition.x, rightClickPosition.y);
+    }
+  });
+  menu.append(menuItem);
 
-const menu = new remote.Menu();
-const menuItem = new MenuItem({
-	label: "Inspect Element",
-	click: () => {
-		remote
-			.getCurrentWindow()
-			.webContents.inspectElement(rightClickPosition.x, rightClickPosition.y);
-	}
-});
-menu.append(menuItem);
-
-window.addEventListener(
-	"contextmenu",
-	e => {
-		e.preventDefault();
-		rightClickPosition = { x: e.x, y: e.y };
-		menu.popup();
-	},
-	false
-);
+  window.addEventListener(
+    "contextmenu",
+    e => {
+      e.preventDefault();
+      rightClickPosition = { x: e.x, y: e.y };
+      menu.popup();
+    },
+    false
+  );
+}
