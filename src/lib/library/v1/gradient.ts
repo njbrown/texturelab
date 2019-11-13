82bbcb5b67b74e80805f6cc5c62d663f45313585
Nicolas Brown <nclsbrwn@gmail.com>
2019-11-12 23:54:37 -0500

    Add Gradient node

---
+++ src/lib/library/v1/gradient.ts
@@ -0,0 +1,95 @@
+import { DesignerNode } from "../../designer/designernode";
+import { Color } from "@/lib/designer/color";
+
+export class GradientNode extends DesignerNode {
+  public init() {
+    this.title = "Gradient";
+
+    this.addColorProperty("colorA", "Color A", Color.parse("#000"));
+    this.addFloatProperty("posA", "Position A", 0, 0, 1, 0.01);
+    this.addColorProperty("colorB", "Color B", Color.parse("#FFF"));
+    this.addFloatProperty("posB", "Position B", 0, 0, 1, 0.01);
+
+    this.addEnumProperty("mode", "Mirror Mode", [
+      "Left To Right",
+      "Right To Left",
+      "Top To Bottom",
+      "Bottom To Top"
+    ]);
+
+    var source = `
+        #define POINTS_MAX 32
+
+        // assumes points are sorted
+        vec3 calcGradient(float t, vec3 colors[POINTS_MAX], float positions[POINTS_MAX], int numPoints)
+        {
+            if (numPoints == 0)
+                return vec3(1,0,0);
+            
+            if (numPoints == 1)
+                return colors[0];
+            
+            // here at least two points are available
+            if (t < positions[0])
+                return colors[0];
+            
+            int last = numPoints - 1;
+            if (t > positions[last])
+                return colors[last];
+            
+            // find two points in-between and lerp
+            
+            for(int i = 0; i < numPoints-1;i++) {
+                if (positions[i+1] > t) {
+                    vec3 colorA = colors[i];
+                    vec3 colorB = colors[i+1];
+                    
+                    float t1 = positions[i];
+                    float t2 = positions[i+1];
+                    
+                    float lerpPos = (t - t1)/(t2 - t1);
+                    return mix(colorA, colorB, lerpPos);
+                    
+                }
+                
+            }
+            
+            return vec3(0,0,0);
+        }
+
+
+        vec4 process(vec2 uv)
+        {
+            float t = 0.0;
+    
+            // left to right
+            if (prop_mode == 0)
+                t = uv.x;
+            // right to left
+            else if (prop_mode == 1)
+                t = 1.0 - uv.x;
+            // top to bottom
+            else if (prop_mode == 2)
+                t = 1.0 - uv.y;
+            // bottom to top
+            else if (prop_mode == 3)
+                t = uv.y;
+
+
+            vec3 colors[POINTS_MAX];
+            colors[0] = prop_colorA.rgb;
+            colors[1] = prop_colorB.rgb;
+            float positions[POINTS_MAX];
+            positions[0] = prop_posA;
+            positions[1] = prop_posB;
+                
+            
+            vec3 col = calcGradient(t, colors, positions, 2);
+            
+            return vec4(col,1.0);
+        }
+        `;
+
+    this.buildShader(source);
+  }
+}
