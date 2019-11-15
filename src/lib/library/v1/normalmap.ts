fa4a8b5ac6fc4b3f27ddc3a4cf2df9a99fe6f2d8
Nicolas Brown <nclsbrwn@gmail.com>
2019-11-15 16:04:08 -0500

    Change normal map default intensity

--- src/lib/library/v1/normalmap.ts
+++ src/lib/library/v1/normalmap.ts
@@ -7,7 +7,7 @@ export class NormalMapNode extends DesignerNode {
     //this.exportName = "result";
     this.addInput("height");
 
-    this.addFloatProperty("strength", "Strength", 0.001, -0.02, 0.02, 0.00001);
+    this.addFloatProperty("strength", "Strength", 0.02, -0.02, 0.02, 0.00001);
 
     var source = `
         vec4 process(vec2 uv)
