be528574829d25aa818eeae7b0fcb97b4365caaa
Nicolas Brown <nclsbrwn@gmail.com>
2020-07-05 03:08:21 -0500

    Add new color picker

--- src/components/properties/ColorProp.vue
+++ src/components/properties/ColorProp.vue
@@ -1,28 +1,45 @@
+,
 <template>
 	<div class="field">
 		<label>{{ prop.displayName }}</label>
 		<div>
-			<input
+			<!-- <input
 				class="color"
 				type="color"
 				:value="prop.value.toHex()"
-				@input="updateValue"
+				@input="onInput"
+				@change="onValue"
+			/> -->
+			<color-picker
+				:value="prop.value.toHex()"
+				@input="onInput"
+				@change="onValue"
 			/>
 		</div>
 	</div>
 </template>
 
 <script lang="ts">
+// https://codepen.io/Brownsugar/pen/NaGPKy
 import { Vue, Prop, Component, Emit } from "vue-property-decorator";
 import { Designer } from "@/lib/designer";
 import { DesignerNode } from "@/lib/designer/designernode";
 import { IPropertyHolder } from "@/lib/designer/properties";
+import ColorPicker from "@/components/ColorPicker.vue";
+import { PropertyChangeAction } from "@/lib/actions/propertychangeaction";
+import { UndoStack } from "@/lib/undostack";
 
-@Component
+@Component({
+	components: {
+		ColorPicker,
+	},
+})
 export default class ColorPropertyView extends Vue {
 	@Prop()
 	// ColorProperty
 	prop: any;
+
+	oldValue: string;
 
 	@Prop()
 	designer: Designer;
@@ -30,15 +47,71 @@ export default class ColorPropertyView extends Vue {
 	@Prop()
 	propHolder: IPropertyHolder;
 
+	public ColorPropertyView() {}
+
+	mounted() {
+		this.oldValue = this.prop.value.toHex();
+	}
+
 	@Emit()
 	propertyChanged() {
 		return this.prop.name;
 	}
 
-	updateValue(evt) {
-		this.propHolder.setProperty(this.prop.name, evt.target.value);
+	onInput(value) {
+		//console.log(value);
+		this.propHolder.setProperty(this.prop.name, value);
+		//this.propertyChanged();
+	}
+
+	onValue(value) {
+		//let oldValue = this.prop.value.toHex();
+		this.propHolder.setProperty(this.prop.name, value);
+
+		let action = new PropertyChangeAction(
+			null,
+			this.prop.name,
+			this.propHolder,
+			this.oldValue,
+			this.prop.value.toHex()
+		);
+		UndoStack.current.push(action);
+
+		// console.log(
+		// 	"color change: " + this.oldValue + " > " + this.prop.value.toHex()
+		// );
+
+		this.oldValue = this.prop.value.toHex();
+
 		this.propertyChanged();
 	}
+
+	// onInput(evt) {
+	// 	this.propHolder.setProperty(this.prop.name, evt.target.value);
+	// 	//this.propertyChanged();
+	// }
+
+	// onValue(evt) {
+	// 	//let oldValue = this.prop.value.toHex();
+	// 	this.propHolder.setProperty(this.prop.name, evt.target.value);
+
+	// 	let action = new PropertyChangeAction(
+	// 		null,
+	// 		this.prop.name,
+	// 		this.propHolder,
+	// 		this.oldValue,
+	// 		this.prop.value.toHex()
+	// 	);
+	// 	UndoStack.current.push(action);
+
+	// 	console.log(
+	// 		"color change: " + this.oldValue + " > " + this.prop.value.toHex()
+	// 	);
+
+	// 	this.oldValue = this.prop.value.toHex();
+
+	// 	this.propertyChanged();
+	// }
 }
 </script>
 
@@ -60,5 +133,14 @@ export default class ColorPropertyView extends Vue {
 	margin-top: 0.4em;
 	width: 100%;
 	width: calc(100% - 4px - 1px);
+	appearance: none;
+}
+
+.color::-webkit-color-swatch-wrapper {
+	padding: 0;
+}
+
+.color::-webkit-color-swatch {
+	border: none;
 }
 </style>
