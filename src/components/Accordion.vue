c9e9c29902c56db4ac0740e247f015ac01e3b0e1
Nicolas Brown <nclsbrwn@gmail.com>
2020-07-10 00:45:30 -0500

    Fix padding in properties ui

--- src/components/Accordion.vue
+++ src/components/Accordion.vue
@@ -1,13 +1,18 @@
 <template>
-  <div class="container">
-    <div
-      @click="collapse(); $event.preventDefault();"
-      class="header disable-select"
-    >{{ isCollapsed?'▼':'▶' }} {{ header }}</div>
-    <div v-show="isCollapsed" class="contents">
-      <slot></slot>
-    </div>
-  </div>
+	<div class="container">
+		<div
+			@click="
+				collapse();
+				$event.preventDefault();
+			"
+			class="header disable-select"
+		>
+			{{ isCollapsed ? "▼" : "▶" }} {{ header }}
+		</div>
+		<div v-show="isCollapsed" class="contents">
+			<slot></slot>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
@@ -15,57 +20,58 @@ import { Vue, Prop, Component, Emit, Model } from "vue-property-decorator";
 
 @Component
 export default class Accordion extends Vue {
-  @Prop({
-    default: "Header"
-  })
-  header: string;
+	@Prop({
+		default: "Header",
+	})
+	header: string;
 
-  @Prop({
-    default: false
-  })
-  collapsed: boolean;
+	@Prop({
+		default: false,
+	})
+	collapsed: boolean;
 
-  showContents: boolean = false;
+	showContents: boolean = false;
 
-  mounted() {
-    this.showContents = this.collapsed;
-  }
+	mounted() {
+		this.showContents = this.collapsed;
+	}
 
-  collapse() {
-    this.showContents = !this.showContents;
-    console.log(this.showContents);
-    console.log(this.isCollapsed);
-  }
+	collapse() {
+		this.showContents = !this.showContents;
+		console.log(this.showContents);
+		console.log(this.isCollapsed);
+	}
 
-  get isCollapsed() {
-    return !this.showContents;
-  }
+	get isCollapsed() {
+		return !this.showContents;
+	}
 }
 </script>
 
 <style scoped>
 .container {
-  margin-bottom: 1px;
+	margin-bottom: 1px;
 }
 .header {
-  color: white;
-  font-weight: bold;
-  font-size: 14px;
-  background: #666;
-  padding: 0.5em;
-  padding-left: 0.5em;
-  cursor: pointer;
+	color: white;
+	font-weight: bold;
+	font-size: 14px;
+	background: #666;
+	padding: 0.5em;
+	padding-left: 0.5em;
+	cursor: pointer;
 }
 
 .contents {
-  margin-left: 1em;
+	margin-left: 0.5em;
+	margin-right: 0.5em;
 }
 
 .disable-select {
-  user-select: none; /* supported by Chrome and Opera */
-  -webkit-user-select: none; /* Safari */
-  -khtml-user-select: none; /* Konqueror HTML */
-  -moz-user-select: none; /* Firefox */
-  -ms-user-select: none; /* Internet Explorer/Edge */
+	user-select: none; /* supported by Chrome and Opera */
+	-webkit-user-select: none; /* Safari */
+	-khtml-user-select: none; /* Konqueror HTML */
+	-moz-user-select: none; /* Firefox */
+	-ms-user-select: none; /* Internet Explorer/Edge */
 }
 </style>
