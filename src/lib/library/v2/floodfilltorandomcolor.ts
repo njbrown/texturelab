ec92024a488576a128b0552f17db51d5794a5ba0
Nicolas Brown <nclsbrwn@gmail.com>
2020-11-26 01:58:24 -0500

    Add variance to floodfill to random color node

--- src/lib/library/v2/floodfilltorandomcolor.ts
+++ src/lib/library/v2/floodfilltorandomcolor.ts
@@ -4,7 +4,8 @@ export class FloodFillToRandomColor extends GpuDesignerNode {
 	public init() {
 		this.title = "Flood Fill To Random Color";
 
-		this.addInput("floodfill");
+        this.addInput("floodfill");
+        this.addIntProperty("variance", "Variance", 30, 0, 60, 1);
 
 		const source = `
         vec2 calcFloodFillOrigin(vec2 uv, vec4 pixelData)
@@ -27,32 +28,17 @@ export class FloodFillToRandomColor extends GpuDesignerNode {
             if (pixelData.ba == vec2(0.0, 0.0))
                 return vec4(0.0, 0.0, 0.0, 1.0);
 
-            //uv = floor(uv * 255.0) / 255.0;
             vec2 center = calcFloodFillCenter(uv, pixelData);
-            // vec2 center = calcFloodFillOrigin(uv, pixelData);
-            // _rand(vec2(_seed) + center * vec2(0.01));
-            //_rand(vec2(_seed) + (brickId + vec2(1) ) * vec2(0.01))
 
             // quantize center to remove minor innaccuracies
             // the hash function is very sensitive to even small changes
-            center = floor(center * 10.0) / 10.0;
+            float variance = float(prop_variance);
+            center = floor(center * variance) / variance;
             
             vec4 color = vec4(0.0, 0.0, 0.0, 1.0);
             color.r = _rand(center + (vec2(1) ) * vec2(0.01));
             color.g = _rand(center + (vec2(2) ) * vec2(0.01));
             color.b = _rand(center + (vec2(3) ) * vec2(0.01));
-
-            // color.r = _rand(center );
-            // color.g = _rand(center );
-            // color.b = _rand(center );
-            // color.rgb = vec3(_rand(center * vec2(0.01)));
-            //color.rgb = vec3(hash12(center));
-            //color.rg = center;
-            
-            
-
-            //color.rgb = vec3(hash12(pixelData.ba));
-            // color.rgb = vec3(hash12(center));
 
             return color;
         }
