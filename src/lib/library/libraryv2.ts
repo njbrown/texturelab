46dbb27775d7905afc5954ba51ee854cd75fb4c3
Nicolas Brown <nclsbrwn@gmail.com>
2021-01-23 18:08:16 -0500

    Add gradient noise fractal sum

--- src/lib/library/libraryv2.ts
+++ src/lib/library/libraryv2.ts
@@ -68,6 +68,7 @@ import { AnisotropicBlur } from "./v2/anisotropicblur";
 import { CircularSplatter } from "./v2/circularsplatter";
 import { ValueNoise } from "./v2/valuenoise";
 import { GradientNoise } from "./v2/gradientnoise";
+import { GradientNoiseFractalSum } from "./v2/gradientnoisefractalsum";
 import { SimplexNoiseV2 } from "./v2/simplexnoise";
 
 export function createLibrary() {
@@ -161,6 +162,7 @@ export function createLibrary() {
 	lib.addNode("circularsplatter", "Circular Splatter", CircularSplatter);
 	lib.addNode("valuenoise", "Value Noise", ValueNoise);
 	lib.addNode("gradientnoise", "Gradient Noise", GradientNoise);
+	lib.addNode("gradientnoisefractalsum", "Gradient Noise Fractal Sum", GradientNoiseFractalSum);
 	//lib.addNode("simplexnoise", "Simplex Noise", SimplexNoiseV2);
 	// lib.addNode("betterwarp", "Better Warp", BetterWarpNode);
 
---
+++ src/lib/library/v2/gradientnoisefractalsum.ts
@@ -0,0 +1,74 @@
+import { GpuDesignerNode } from "../../designer/gpudesignernode";
+
+// https://www.shadertoy.com/view/XdXGW8
+export class GradientNoiseFractalSum extends GpuDesignerNode {
+	public init() {
+		this.title = "Gradient Noise Fractal Sum";
+
+        this.addIntProperty("scale", "Scale", 8, 1, 100, 1);
+        this.addIntProperty("layers", "Layers", 5, 1, 20, 1);
+        this.addFloatProperty("gain", "Gain", 0.5, 0.1, 2, 0.1);
+        this.addFloatProperty("lacunarity", "Lacunarity", 0.5, 0.1, 2, 0.1);
+        
+
+        const source = `
+        vec2 hash( vec2 x )  // replace this by something better
+        {
+            const vec2 k = vec2( 0.3183099, 0.3678794 );
+            x = x*k + k.yx;
+            return -1.0 + 2.0*fract( 16.0 * k*fract( x.x*x.y*(x.x+x.y)) );
+        }
+
+        float wrapAround(float value, float upperBound) {
+            return mod((value + upperBound - 1.0), upperBound);
+        }
+
+        vec2 wrapAndHash(vec2 value, vec2 upperBounds) {
+            value.x = wrapAround(value.x, upperBounds.x);
+            value.y = wrapAround(value.y, upperBounds.y);
+
+            return -1.0 + 2.0 * hash22(value + vec2(_seed));
+        }
+
+        float noise( in vec2 p )
+        {
+            vec2 i = floor( p );
+            vec2 f = fract( p );
+            
+            vec2 u = f*f*(3.0-2.0*f);
+
+            vec2 bounds = vec2(float(prop_scale));
+
+            return mix( mix( dot( wrapAndHash( i + vec2(0.0,0.0), bounds ), f - vec2(0.0,0.0) ), 
+                     dot( wrapAndHash( i + vec2(1.0,0.0), bounds ), f - vec2(1.0,0.0) ), u.x),
+                mix( dot( wrapAndHash( i + vec2(0.0,1.0), bounds ), f - vec2(0.0,1.0) ), 
+                     dot( wrapAndHash( i + vec2(1.0,1.0), bounds ), f - vec2(1.0,1.0) ), u.x), u.y);
+        }
+
+        vec4 process(vec2 uv)
+        {
+
+            float scale = float(prop_scale);
+            float amplitude = 1.0;
+
+            float sum = 0.0;
+
+            for(int i = 0; i < prop_layers; i++) {
+                sum += noise(uv * float(scale)) * amplitude;
+
+                scale *= prop_lacunarity;
+                amplitude *= prop_gain;
+            }
+            
+            // adjust range
+            sum = 0.5 + 0.5 * sum;
+            
+            vec3 color = vec3(sum);
+
+            return vec4(color,1.0);
+        }
+        `;
+
+		this.buildShader(source);
+	}
+}
