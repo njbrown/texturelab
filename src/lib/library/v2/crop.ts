108ab918ba2c3e8fbe2533962e7081ae57f290ef
Leonardo Schwarz <mail@leoschwarz.com>
2021-05-30 15:30:47 +0200

    Crop node: wrap around and customizable bg color

--- src/lib/library/v2/crop.ts
+++ src/lib/library/v2/crop.ts
@@ -1,3 +1,4 @@
+import { Color } from "@/lib/designer/color";
 import { GpuDesignerNode } from "@/lib/designer/gpudesignernode";
 
 export class Crop extends GpuDesignerNode {
@@ -11,16 +12,26 @@ export class Crop extends GpuDesignerNode {
 		this.addFloatProperty("max_x", "Max X", 1.0, 0, 1, 0.01);
 		this.addFloatProperty("max_y", "Max Y", 1.0, 0, 1, 0.01);
 
+		this.addColorProperty("bg", "Background Color", new Color());
+
 		const source = `
 		
+		bool is_fg(float x, float min_x, float max_x)
+		{
+			if (min_x <= max_x) {
+				return min_x <= x && x <= max_x;
+			} else {
+				return x <= max_x || min_x <= x;
+			}
+		}
+
 		vec4 process(vec2 uv)
 		{
-			if (prop_min_x <= uv.x && uv.x <= prop_max_x &&
-			    prop_min_y <= uv.y && uv.y <= prop_max_y)
+			if (is_fg(uv.x, prop_min_x, prop_max_x) && is_fg(uv.y, prop_min_y, prop_max_y))
 			{
 				return texture(image, uv);
 			} else {
-				return vec4(0,0,0,1);
+				return vec4(prop_bg.rgb, 1.0);
 			}
 		}
 		`;
