b6c57345112d45e4d305809167bacc07cca70db8
Nicolas Brown <nclsbrwn@gmail.com>
2021-09-29 09:41:19 -0500

    output channel specified in props for output node

--- src/lib/library/v1/output.ts
+++ src/lib/library/v1/output.ts
@@ -29,11 +29,16 @@ export class OutputNode extends GpuDesignerNode {
             if (image_connected) {
               col = vec4(0,1,0,1);
               col = texture(image, uv);
-              return texture(image, uv);
+            //   return texture(image, uv);
             } else {
               col = prop_color;
-              return prop_color;
+            //   return prop_color;
             }
+
+			if(prop_components == 2) col.rgb = vec3(col.r);
+			if(prop_components == 3) col.rgb = vec3(col.g);
+			if(prop_components == 4) col.rgb = vec3(col.b);
+			if(prop_components == 5) col.rgb = vec3(col.a);
 
             return col;
         }
