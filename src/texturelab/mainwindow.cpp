d5a7024708021eb2b1e8ef3aa33b1104f29c2bf8
Nicolas Brown <nclsbrwn@gmail.com>
2022-09-03 00:07:18 -0400

    setup docks

--- src/texturelab/mainwindow.cpp
+++ src/texturelab/mainwindow.cpp
@@ -1,10 +1,73 @@
 #include "mainwindow.h"
 
+#include <QList>
+
+#include "DockSplitter.h"
+#include "DockAreaWidget.h"
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
-
     resize(1280, 720);
+
+    // this->setupMenus();
+
+    this->dockManager = new ads::CDockManager(this);
+
+    this->setupDocks();
+}
+
+/*
+    https://github.com/githubuser0xFFFF/Qt-Advanced-Docking-System/issues/411
+    https://github.com/githubuser0xFFFF/Qt-Advanced-Docking-System/issues/340
+
+    pass ratios as fractions, the function handles the actual width calculation
+    setWidgetRatiosInArea(myArea, { 1.0f/5.0f, 1.0f/5.0f, 1.0f/5.0f })
+*/
+void setWidgetRatiosInArea(ads::CDockAreaWidget *area, const QList<float> &ratios)
+{
+    auto splitter = ads::internal::findParent<ads::CDockSplitter *>(area);
+    if (splitter)
+    {
+        int width = splitter->width();
+
+        QList<int> finalRatios;
+        for (auto ratio : ratios)
+        {
+            finalRatios.append(ratio * width);
+        }
+        splitter->setSizes(finalRatios);
+    }
+}
+
+void MainWindow::setupDocks()
+{
+    setDockNestingEnabled(true);
+
+    // https://forum.qt.io/topic/3055/mainwindow-layout-problem-with-qdockwidget/17
+
+    // graph goes in the center
+    auto graphArea = addDock("Graph", ads::CenterDockWidgetArea, new QWidget(this), nullptr);
+    auto leftArea = addDock("2D View", ads::LeftDockWidgetArea, new QWidget(this), graphArea);
+    auto rightArea = addDock("Properties", ads::RightDockWidgetArea, new QWidget(this), graphArea);
+
+    setWidgetRatiosInArea(graphArea, {1.0f / 5, 3.0f / 5, 1.0f / 5});
+
+    addDock("3D View", ads::BottomDockWidgetArea, new QWidget(this), leftArea);
+    addDock("Library", ads::BottomDockWidgetArea, new QWidget(this), rightArea);
+    setWidgetRatiosInArea(leftArea, {0.5f, 0.5f});
+    setWidgetRatiosInArea(rightArea, {0.5f, 0.5f});
+}
+
+ads::CDockAreaWidget *MainWindow::addDock(const QString &title, ads::DockWidgetArea area, QWidget *widget, ads::CDockAreaWidget *areaWidget)
+{
+    ads::CDockWidget *dockWidget = new ads::CDockWidget(title);
+    if (widget != nullptr)
+        dockWidget->setWidget(widget);
+
+    auto newAreaWidget = dockManager->addDockWidget(area, dockWidget, areaWidget);
+
+    return newAreaWidget;
 }
 
 MainWindow::~MainWindow()
--- src/texturelab/mainwindow.h
+++ src/texturelab/mainwindow.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 #include <QString>
+#include "DockManager.h"
 
 class MainWindow : public QMainWindow
 {
@@ -13,5 +14,12 @@ public:
     ~MainWindow();
 
 protected:
+    void setupMenus();
+    void setupDocks();
+
+    ads::CDockAreaWidget *addDock(const QString &title, ads::DockWidgetArea area, QWidget *widget, ads::CDockAreaWidget *areaWidget);
+
+private:
+    ads::CDockManager *dockManager;
 };
 #endif // MAINWINDOW_H
