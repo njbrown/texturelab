870952424993421c1d715d6b7a4d9f4b5cde65bd
Nicolas <nclsbrwn@gmail.com>
2025-12-27 12:48:55 -0500

    hint preferred gpu on windows

--- src/texturelab/main.cpp
+++ src/texturelab/main.cpp
@@ -2,9 +2,20 @@
 
 #include <QApplication>
 
+// Hints that a dedicated GPU should be used whenever possible
+// https://stackoverflow.com/a/39047129/991834
+#ifdef Q_OS_WIN
+extern "C"
+{
+  __declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
+  __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
+#endif
+
 int main(int argc, char* argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
+    QCoreApplication::setAttribute(Qt::AA_UseDesktopOpenGL);
     QApplication a(argc, argv);
     MainWindow w;
 
