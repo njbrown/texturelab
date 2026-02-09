4eb6840047ed42a52afe18cf51161edc50e0b908
Nicolas Brown <nclsbrwn@gmail.com>
2026-02-08 19:12:52 -0500

    add defs for nudging windows to use dedicated GPUs

--- src/texturelab/main.cpp
+++ src/texturelab/main.cpp
@@ -6,10 +6,9 @@
 // Hints that a dedicated GPU should be used whenever possible
 // https://stackoverflow.com/a/39047129/991834
 #ifdef Q_OS_WIN
-extern "C"
-{
-  __declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
-  __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+extern "C" {
+__declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
+__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
 }
 #endif
 
@@ -21,8 +20,8 @@ int main(int argc, char* argv[])
     QSurfaceFormat format;
     format.setProfile(QSurfaceFormat::CoreProfile);
     format.setVersion(3, 2);
+    // format.setColorSpace(QSurfaceFormat::sRGBColorSpace);
     QSurfaceFormat::setDefaultFormat(format);
-
 
     QApplication a(argc, argv);
     MainWindow w;
